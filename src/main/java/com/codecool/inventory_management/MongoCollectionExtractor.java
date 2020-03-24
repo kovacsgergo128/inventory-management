a58173181991c1729fa23427f8b5edf5f1241ef1
Gergo Kovacs <kovacsgergo128@gmail.com>
2020-03-24 18:52:21 +0100

    Add a generic List extractor of db queries return MongoCollection

---
+++ src/main/java/com/codecool/inventory_management/MongoCollectionExtractor.java
@@ -0,0 +1,18 @@
+package com.codecool.inventory_management;
+
+import com.mongodb.client.FindIterable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MongoCollectionExtractor {
+
+    public static <E> List<E> extract(FindIterable<E> queryResult) {
+        List<E> result = new ArrayList<>();
+        for (E element : queryResult) {
+            result.add(element);
+        }
+
+        return result;
+    }
+}
--- src/main/java/com/codecool/inventory_management/TransactionDao.java
+++ src/main/java/com/codecool/inventory_management/TransactionDao.java
@@ -1,27 +1,16 @@
 package com.codecool.inventory_management;
 
-import com.mongodb.MongoClientSettings;
 import com.mongodb.client.FindIterable;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
-import org.bson.codecs.configuration.CodecProvider;
-import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.codecs.pojo.PojoCodecProvider;
 import org.bson.types.ObjectId;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
-import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class TransactionDao {
     private static TransactionDao transactionDao = null;
 
     private MongoDatabase connection;
-
 
     private TransactionDao() {
         this.connection = ConnectionHandler.getInstance().getDatabase();
@@ -35,14 +24,27 @@ public class TransactionDao {
     }
 
     public List<Transaction> getAllTransactionsOf(ObjectId inventoryId) {
-        List<Transaction> result = new ArrayList<>();
         FindIterable<Transaction> transactions =
                 connection.getCollection("Transaction")
                           .withDocumentClass(Transaction.class)
                           .find(Filters.eq("inventoryId", inventoryId));
 
-        for (Transaction transaction : transactions) result.add(transaction);
+        return MongoCollectionExtractor.extract(transactions);
+    }
 
-        return result;
+    public Transaction getTransactionBy(ObjectId transactionId) {
+        return connection.getCollection("Transaction")
+                         .withDocumentClass(Transaction.class)
+                         .find(Filters.eq("id", transactionId))
+                         .first();
+    }
+
+    public List<Transaction> getAllTransactions() {
+        FindIterable<Transaction> transactions =
+                connection.getCollection("Transaction")
+                          .withDocumentClass(Transaction.class)
+                          .find();
+
+        return MongoCollectionExtractor.extract(transactions);
     }
 }
