9a789975f1104c819a04be1b59b704fe2610460d
Rajnai Máté <ramate.rajnai@gmail.com>
2020-03-25 16:40:50 +0100

    product category

---
+++ src/main/java/com/codecool/inventory_management/dao/ProductCategoryDao.java
@@ -0,0 +1,35 @@
+package com.codecool.inventory_management.dao;
+
+import com.codecool.inventory_management.model.ProductCategory;
+import com.codecool.inventory_management.util.ConnectionHandler;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class ProductCategoryDao {
+
+    private static ProductCategoryDao instance = null;
+    private static final String COLLECTION_NAME = "ProductCategory";
+
+    private MongoDatabase connection;
+    private MongoCollection<ProductCategory> collection;
+
+    private ProductCategoryDao() {
+        this.connection = ConnectionHandler.getInstance().getDatabase();
+        this.collection = connection.getCollection(COLLECTION_NAME, ProductCategory.class);
+    }
+
+    public static ProductCategoryDao getInstance() {
+        if (instance == null)
+            instance = new ProductCategoryDao();
+
+        return instance;
+    }
+
+    public void add(ProductCategory productCategory) {
+        collection.insertOne(productCategory);
+    }
+
+
+
+
+}
