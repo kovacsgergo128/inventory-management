9d2608106ee6ade8937c249de7c7374bce33738f
Rajnai Máté <ramate.rajnai@gmail.com>
2020-03-25 13:56:57 +0100

    test remove product from db

--- src/test/java/com/codecool/inventory_management/dao/ProductDaoTest.java
+++ src/test/java/com/codecool/inventory_management/dao/ProductDaoTest.java
@@ -17,6 +17,8 @@ class ProductDaoTest {
         this.productDao = ProductDao.getInstance();
     }
 
+
+
     @Test
     public void addProductToDatabaseTest() {
         ProductCategory productCategory1 = new ProductCategory(new ObjectId("5e78fe2a1b65c45a7b03baa2"), "Fruit");
@@ -27,4 +29,22 @@ class ProductDaoTest {
         assertNotNull(productDao.getProductBy(newProductId));
 
     }
+
+    // QUESTION:
+    // How can I avoid to create a product for the test case below too?
+    // I would like to use the product id created in addTest and remove that product from the database.
+    // Or is it impossible because test cases are independent from each other?
+    // Solution: Should I create a Constructor for testing purposes?
+    @Test
+    public void removeProductFromDatabaseTest() {
+        ProductCategory productCategory1 = new ProductCategory(new ObjectId("5e78fe2a1b65c45a7b03baa2"), "Fruit");
+        Product product1 = new Product(9, "Test product 1", 100, 160, productCategory1);
+        ObjectId newProductId = product1.getId();
+        System.out.println(newProductId);
+        productDao.add(product1);
+        assertNotNull(productDao.getProductBy(newProductId));
+        productDao.remove(newProductId);
+        assertNull(productDao.getProductBy(newProductId));
+    }
+
 }
