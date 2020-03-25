10ad51ea00d66b5666984f46c604a3131edfe778
Rajnai Máté <ramate.rajnai@gmail.com>
2020-03-25 11:38:27 +0100

    add product to database test

---
+++ src/test/java/com/codecool/inventory_management/dao/ProductDaoTest.java
@@ -0,0 +1,28 @@
+package com.codecool.inventory_management.dao;
+
+import com.codecool.inventory_management.model.Product;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductDaoTest {
+
+    private ProductDao productDao = null;
+
+    @BeforeEach
+    public void init() {
+        this.productDao = ProductDao.getInstance();
+    }
+
+    @Test
+    public void addProductToDatabaseCorrectly() {
+        Product product1 = new Product(9, "Test product 1", 100, 160, new ObjectId("5e78fe2a1b65c45a7b03baa2"));
+        ObjectId newProductId = product1.getId();
+        assertNull(productDao.getProductBy(newProductId));
+        productDao.add(product1);
+        assertNotNull(productDao.getProductBy(newProductId));
+
+    }
+}
