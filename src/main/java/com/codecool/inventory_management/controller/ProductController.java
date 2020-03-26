fe75048ba508968204f8047eb4576053ef2f7fcc
Gergo Kovacs <kovacsgergo128@gmail.com>
2020-03-26 10:45:57 +0100

    Add ProductController and implement its basic function - returns all products available

---
+++ src/main/java/com/codecool/inventory_management/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.codecool.inventory_management.controller;
+
+import com.codecool.inventory_management.dao.ProductDao;
+import com.codecool.inventory_management.util.JsonProvider;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/products/*")
+public class ProductController extends HttpServlet {
+
+    private ProductDao productDao = ProductDao.getInstance();
+    private JsonProvider jsonProvider = new JsonProvider();
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        jsonProvider.sendJson(resp, jsonProvider.stringify(productDao.getAllProducts()));
+    }
+
+}
